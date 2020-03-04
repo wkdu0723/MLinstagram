b0c7e3927bccba762edc4c8a2cb2827f396e3ca1
wkdu0723 <57204674+wkdu0723@users.noreply.github.com>
2020-03-05 04:37:13 +0900

    마이페이지에 좋아요 누른 게시물 가져오기

--- app/src/main/java/com/ml/instagramapp/navigation/UserFragment.kt
+++ app/src/main/java/com/ml/instagramapp/navigation/UserFragment.kt
@@ -6,17 +6,77 @@ import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.ml.instagramapp.R
+import com.ml.instagramapp.navigation.model.ContentDTO
+import kotlinx.android.synthetic.main.fragment_user.view.*
 
 class UserFragment : Fragment() {
+    var fragmentView : View? = null
+    var firestore : FirebaseFirestore? = null
+    var uid : String? = null
+    var auth : FirebaseAuth? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
-        return view;
+        fragmentView  = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
+        uid = arguments?.getString("destinationUid")
+        firestore = FirebaseFirestore.getInstance() //초기화
+        auth = FirebaseAuth.getInstance() //초기화
+
+        fragmentView?.account_reyclerview?.adapter = UserFragmentRecyclerViewAdapter() //어뎁터를 달아줌
+        fragmentView?.account_reyclerview?.layoutManager = GridLayoutManager(activity!!, 3)
+
+        return fragmentView;
+    }
+    inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+
+        //데이터베이스 값들을 읽어옴
+        init{
+            firestore?.collection("images")?.whereEqualTo("uid",uid)?.addSnapshotListener{querySnapshot, firebaseFirestoreException ->
+                if(querySnapshot == null) return@addSnapshotListener
+
+                //데이터 받아옴
+                for(snapshot in querySnapshot.documents){
+                    contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                }
+                fragmentView?.account_tv_post_count?.text = contentDTOs.size.toString()
+                notifyDataSetChanged()
+
+            }
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var width = resources.displayMetrics.widthPixels / 3
+            var imageview = ImageView(parent.context)
+            imageview.layoutParams = LinearLayoutCompat.LayoutParams(width,width)
+            return CustomViewHolder(imageview)
+        }
+
+        inner class CustomViewHolder(var imageview: ImageView) : RecyclerView.ViewHolder(imageview) {
+
+        }
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var imageview = (holder as CustomViewHolder).imageview
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl).apply(RequestOptions().centerCrop()).into(imageview) //centerCrop은 imageview크기에 맞춰 이미즈를 중앙에 받아옴
+        }
     }
 }
